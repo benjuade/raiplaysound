554b9933ac751a2a92346a77fd1765a701d836ad
benjuade <222404684+benjuade@users.noreply.github.com>
2026-05-05 17:41:09 +0200

    add wikiradiolevocidellastoria

--- update.sh
+++ update.sh
@@ -2,7 +2,8 @@
 urls=("https://www.raiplaysound.it/programmi/codicebeta-ognicosaedigitale" 
      "https://www.raiplaysound.it/programmi/etabeta"
      "https://www.raiplaysound.it/programmi/pilloledietabeta"
-     "https://www.raiplaysound.it/programmi/radio3scienza")
+     "https://www.raiplaysound.it/programmi/radio3scienza"
+     "https://www.raiplaysound.it/programmi/wikiradiolevocidellastoria")
 
 for url in "${urls[@]}"; do
     raiplaysound single "$url" --folder programmi --dateok --skip "informazione notiziari radiocronache programmitv film fiction serietv"
