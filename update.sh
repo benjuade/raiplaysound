c74fbe3bce8d399af01a93e6b45aca497f60ad3a
benjuade <222404684+benjuade@users.noreply.github.com>
2026-05-15 08:24:13 +0200

    2026-05-15 update

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 urls=("https://www.raiplaysound.it/programmi/codicebeta-ognicosaedigitale" 
      "https://www.raiplaysound.it/programmi/etabeta"
+     "https://www.raiplaysound.it/programmi/padronidelmondo"
      "https://www.raiplaysound.it/programmi/pilloledietabeta"
      "https://www.raiplaysound.it/programmi/radio3scienza"
      "https://www.raiplaysound.it/programmi/wikiradiolevocidellastoria")
